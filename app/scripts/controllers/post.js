473874504a774cb89dd5c76ba467266a7e1e2764
Alan Broun <abroun@dawnrobotics.co.uk>
2015-06-21 16:10:40 +0100

    Finished styling the pages and fixed a small display bug in the post view.

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -9,10 +9,16 @@
  */
 angular.module('potFetApp')
   .controller('PostCtrl', function ($scope, $http, $routeParams, helperMethods, FLICKR_PROXY_URL) {
+    
+    $scope.items = [];
+    $scope.item = {};
+    $scope.loaded = false;
+      
     $http.get(FLICKR_PROXY_URL).success(function(data) {
       $scope.items = data.items;
       $scope.postId = $routeParams.postId;
       $scope.item = $scope.items[ $scope.postId ];
+      $scope.loaded = true;
     });
       
     $scope.formatPublishedDate = helperMethods.formatPublishedDate;
